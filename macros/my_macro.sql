94d0d1a1a4c0f8ad2325b479db0963a604189ba9
Liran <liran>
2025-07-30 14:56:16 +0300

    ,

--- macros/my_macro.sql
+++ macros/my_macro.sql
@@ -3,3 +3,13 @@
     null as {{name}}
 
 {% endmacro %}
+
+
+
+{% macro date_formatter(col) %}
+    coalesce(
+        try_to_date({{ col }}, 'YYYY-MM-DD'),
+        try_to_date({{ col }}, 'MM/DD/YYYY'),
+        try_to_date({{ col }}, 'DD-MM-YYYY')
+    )
+{% endmacro %}
